a7073b25eb4c997fdef9c333adbcd44715b1df08
Ioannis Fytros <rinenweb@gmail.com>
2025-03-07 15:05:58 +0200

    Add files via upload

--- mod_horizontal_timeline/tmpl/default.php
+++ mod_horizontal_timeline/tmpl/default.php
@@ -1,6 +1,8 @@
 <?php
 // Prevent direct access
 defined('_JEXEC') or die;
+use Joomla\CMS\Router\Route;
+
 $document = JFactory::getDocument();
 $document->addScript(JURI::base() . 'modules/mod_horizontal_timeline/tmpl/script.js');
 //$document->addScript('./tmpl/sample.js');
@@ -9,6 +11,9 @@ $document->addScript(JURI::base() . 'modules/mod_horizontal_timeline/tmpl/script
 //echo "#" . gettype($timelineItems);
 // Check if there are timeline items
 $numberOfColumns = count((array)$timelineItems);
+$selectedItemId = '';
+
+$sefUrl = '';
 ?>
 
 <link rel="stylesheet" href="style.css">
@@ -19,30 +24,37 @@ $numberOfColumns = count((array)$timelineItems);
 </style>
 
 <?php if (!empty($aboveText)) : ?>
-    <div class="timeline-above">
-        <?php echo $aboveText; ?>
-    </div>
+  <div class="timeline-above">
+    <?php echo $aboveText ?>
+  </div>
 <?php endif; ?>
 
 <?php if (!empty($timelineItems)): ?>
 	<div class="timelinebody">
 		<div class="moa-roadmap-group">
 			<?php foreach ($timelineItems as $index => $item): ?>
-			  <?php $jsonData = htmlspecialchars(json_encode($item->options), ENT_QUOTES, 'UTF-8');?>
-				<div class="moa-roadmap-wrapper" id='<?php echo "moa-roadmap-wrapper-".$index ?>' data-event="<?php echo $jsonData ?>" onclick="showmessage('<?php echo $index?>')">
-				  <div class="moa-roadmap-card" id='<?php echo "moa-roadmap-card-".$index ?>'>                    
+        <?php
+          $itemsDescription = array();
+          $itemDescription[$index] = $item->options->below_text;          
+        ?>
+				<div class="moa-roadmap-wrapper" id='<?php echo "moa-roadmap-wrapper-".$index ?>' onclick="showmessage('<?php echo $index?>', '<?php echo $numberOfColumns?>')">
+				  <div class="moa-roadmap-card" id='<?php echo "moa-roadmap-card-".$index ?>'>
             <div class="moa-roadmap-card-title" id='<?php echo "moa-roadmap-card-title-".$index ?>'><?php echo $item->options->above_dot_text ?></div>
 				  </div>
           <div class="moa-roadmap-card-footer" id='<?php echo "moa-roadmap-card-footer-".$index ?>'>
             <span class="moa-roadmap-card-year" id='<?php echo "moa-roadmap-card-year-".$index ?>'><?php echo $item->options->year ?></span>
           </div>
-				</div>				
+				</div>
 			<?php endforeach; ?>
 		</div>
 	</div>
 	<!-- Area for showing Text Below Timeline dynamically -->
-    <div class="moa-roadmap-details">
-        <div id="moa-roadmap-details-content"></div>
+    <div class="moa-roadmap-details">      
+      <?php foreach ($itemDescription as $index => $item): ?>
+        <div id='<?php echo "moa-roadmap-details-content-" . $index ?>' class="moa-roadmap-details-content-hidden">
+          <?php echo $item ?>
+        </div>
+      <?php endforeach; ?>
     </div>
 
 <?php else: ?>
--- mod_horizontal_timeline/tmpl/script.js
+++ mod_horizontal_timeline/tmpl/script.js
@@ -1,27 +1,115 @@
-function showmessage(id) {
+function showmessage(id, numberOfElements) {
 
   // set title active
   const items = document.getElementsByClassName('moa-roadmap-card-title');
   for (let index = 0; index < items.length; index++)
     items[index].classList.remove("active");
   var dataContainer = document.getElementById("moa-roadmap-card-title-" + id);
-  dataContainer.classList.add('active');  
+  dataContainer.classList.add('active');
 
   // set year active
   const items2 = document.getElementsByClassName('moa-roadmap-card-year');
   for (let index = 0; index < items2.length; index++)
     items2[index].classList.remove("active");
-  var dataContainer = document.getElementById("moa-roadmap-card-year-" + id);  
+  var dataContainer = document.getElementById("moa-roadmap-card-year-" + id);
   dataContainer.classList.add('active');
 
   const items3 = document.getElementsByClassName('moa-roadmap-card-footer');
   for (let index = 0; index < items3.length; index++)
     items3[index].classList.remove("active");
   document.getElementById("moa-roadmap-card-footer-" + id).classList.add('active');
+  
+  //NEW APPOACH
+   for (var i=0;i < numberOfElements;i++) {     
+      var extractedId = id.replace('timeline_items', '');
+      if (i.toString() === extractedId.toString()) {
+        const moaRoadmapDetailsContent = document.getElementById('moa-roadmap-details-content-timeline_items' + i.toString());
+        moaRoadmapDetailsContent.classList.remove('moa-roadmap-details-content-hidden');
+        moaRoadmapDetailsContent.classList.add('moa-roadmap-details-content-visible');
+      }
+      else 
+      {
+        const moaRoadmapDetailsContent = document.getElementById('moa-roadmap-details-content-timeline_items' + i.toString());
+        moaRoadmapDetailsContent.classList.remove('moa-roadmap-details-content-visible');
+        moaRoadmapDetailsContent.classList.add('moa-roadmap-details-content-hidden');
+      }
+   }
 
-  // set below text
-  var dataContainer = document.getElementById("moa-roadmap-wrapper-" + id);
-  var userData = JSON.parse(dataContainer.dataset.event);
-  var timelineDetailsContent = document.getElementById('moa-roadmap-details-content');
-  timelineDetailsContent.innerHTML = userData.below_text;
+  //OLD APPROACH
+  //var userData = JSON.parse(document.getElementById("moa-roadmap-wrapper-" + id).dataset.event);
+  //document.getElementById('moa-roadmap-details-content').innerHTML = modifyHrefInHtmlString(userData);  
+}
+
+function modifyHrefInHtmlString(htmlString) {
+  //Create a temporary DOM element
+  var tempDiv = document.createElement("div");
+  tempDiv.innerHTML = htmlString;
+
+  // Select all anchor elements
+  var links = tempDiv.querySelectorAll("a");
+
+  // Iterate through the links and modify the href attributes
+  links.forEach(function (link) {
+    if (urlHasOptionContent(link.href) === true)
+      link.href = extractContent(link.href);    
+  });
+
+  return tempDiv.innerHTML;
+}
+
+function urlHasOptionContent(url) {
+  var ret = false;
+  var params = new URLSearchParams(url.split('?')[1]);
+  if (params.get('option') === 'com_content') // Get the 'id' parameter
+    ret = true;
+
+  return ret;
+}
+
+function extractContent(url) {
+  var params = new URLSearchParams(url.split('?')[1]);
+  var idParam = params.get('id');
+  var catIdParam = params.get('catid');
+  var extractedText = '/' + catIdParam.split(':')[1] + '/' + idParam.split(':')[1];
+  return extractedText;
+}
+
+//TAGS example
+//index.php?option=com_tags&view=tag&id=3:diavouleusi-se-exelixi&Itemid=130
+
+function urlHasOptionTag(url) {
+  var ret = false;
+  var params = new URLSearchParams(url.split('?')[1]);
+  if (params.get('option') === 'com_tags') // Get the 'id' parameter
+    ret = true;
+
+  return ret;
+}
+
+function extractTag(url) {
+  return '<?php echo ' + url + '); ?>';
+  //   console.log(result);
+
+  // var params = new URLSearchParams(url.split('?')[1]);  
+  // return '/' + params.get('id').split(':')[1];
+}
+
+function processString(inputString) {
+  fetch('process_string.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'inputString=' + encodeURIComponent(inputString)
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.result) {
+        return data.result;
+        //console.log('Processed string:', data.result);
+      } else {
+        console.error('Error:', data.error);
+      }
+    })
+    .catch(error => console.error('Error:', error));
 }
