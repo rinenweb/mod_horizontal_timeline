04fe1acaa9ea2bb6fc72ecaa89b451b1bbb4d804
Ioannis Fytros <rinenweb@gmail.com>
2026-09-15 12:06:24 +0300

    Refactor asset loading in mod_horizontal_timeline
    Updated the module to use Joomla's Web Asset Manager for asset loading.

--- mod_horizontal_timeline/mod_horizontal_timeline.php
+++ mod_horizontal_timeline/mod_horizontal_timeline.php
@@ -1,15 +1,36 @@
 <?php
+/**
+ * @package     Rinenweb.Module
+ * @subpackage  mod_horizontal_timeline
+ * @author      Rinenweb <info@rinenweb.eu>
+ * @link        https://www.rinenweb.eu
+ * @license     GNU General Public License version 2 or later
+ */
+
 // Prevent direct access
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
+
+/** @var \Joomla\CMS\Application\CMSApplicationInterface $app */
+$app = Factory::getApplication();
+$wa  = $app->getDocument()->getWebAssetManager();
+
 // Load the module parameters
-$moduleId = $module->id;
-$aboveText = $params->get('above_text', '');
+$aboveText     = $params->get('above_text', '');
 $timelineItems = $params->get('timeline_items', []);
-$document = JFactory::getDocument();
 
-// Add CSS
-$document->addStyleSheet('modules/mod_horizontal_timeline/tmpl/style.css');
+// Register and load the assets once, via the Web Asset Manager
+$wa->registerAndUseStyle(
+    'mod_horizontal_timeline',
+    'modules/mod_horizontal_timeline/tmpl/style.css'
+);
+$wa->registerAndUseScript(
+    'mod_horizontal_timeline',
+    'modules/mod_horizontal_timeline/tmpl/script.js',
+    [],
+    ['defer' => true]
+);
 
-require JModuleHelper::getLayoutPath('mod_horizontal_timeline');
-?>
+require ModuleHelper::getLayoutPath('mod_horizontal_timeline');
