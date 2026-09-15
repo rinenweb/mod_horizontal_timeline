f6cf51dc026dbce472fb7f2e3d4a7b788a27716b
Ioannis Fytros <rinenweb@gmail.com>
2026-09-15 12:08:28 +0300

    Refactor timeline module template for better structure

--- mod_horizontal_timeline/tmpl/default.php
+++ mod_horizontal_timeline/tmpl/default.php
@@ -1,62 +1,59 @@
 <?php
 // Prevent direct access
 defined('_JEXEC') or die;
-use Joomla\CMS\Router\Route;
 
-$document = JFactory::getDocument();
-$document->addScript(JURI::base() . 'modules/mod_horizontal_timeline/tmpl/script.js');
-//$document->addScript('./tmpl/sample.js');
-// Get the timeline items from module parameters
-//$timelineItems = $params->get('timeline_items', []);
-//echo "#" . gettype($timelineItems);
-// Check if there are timeline items
-$numberOfColumns = count((array)$timelineItems);
-$selectedItemId = '';
+use Joomla\CMS\Language\Text;
 
-$sefUrl = '';
+$moduleId = (int) $module->id;
+
+// Build the list of "below timeline" descriptions, keyed by item index
+$descriptions = [];
+
+foreach ((array) $timelineItems as $index => $item) {
+    $descriptions[$index] = $item->options->below_text ?? '';
+}
+
+$numberOfColumns = count((array) $timelineItems);
 ?>
-
-<link rel="stylesheet" href="modules/mod_horizontal_timeline/tmpl/style.css">
 <style>
-  :root {
-    --colNum: <?=$numberOfColumns?>;    
-  }
+  #mod-htl-<?php echo $moduleId; ?> { --colNum: <?php echo $numberOfColumns; ?>; }
 </style>
 
+<div id="mod-htl-<?php echo $moduleId; ?>" class="mod-horizontal-timeline">
 <?php if (!empty($aboveText)) : ?>
   <div class="timeline-above">
-    <?php echo $aboveText ?>
+    <?php echo $aboveText; ?>
   </div>
 <?php endif; ?>
 
-<?php if (!empty($timelineItems)): ?>
-	<div class="timelinebody">
-		<div class="moa-roadmap-group">
-			<?php foreach ($timelineItems as $index => $item): ?>
-        <?php
-          $itemsDescription = array();
-          $itemDescription[$index] = $item->options->below_text;          
-        ?>
-				<div class="moa-roadmap-wrapper" id='<?php echo "moa-roadmap-wrapper-".$index ?>' onclick="showmessage('<?php echo $index?>', '<?php echo $numberOfColumns?>')">
-				  <div class="moa-roadmap-card" id='<?php echo "moa-roadmap-card-".$index ?>'>
-            <div class="moa-roadmap-card-title" id='<?php echo "moa-roadmap-card-title-".$index ?>'><?php echo $item->options->above_dot_text ?></div>
-				  </div>
-          <div class="moa-roadmap-card-footer" id='<?php echo "moa-roadmap-card-footer-".$index ?>'>
-            <span class="moa-roadmap-card-year" id='<?php echo "moa-roadmap-card-year-".$index ?>'><?php echo $item->options->year ?></span>
+<?php if (!empty($timelineItems)) : ?>
+  <div class="timelinebody">
+    <div class="moa-roadmap-group">
+      <?php foreach ($timelineItems as $index => $item) : ?>
+        <div class="moa-roadmap-wrapper"
+             id="moa-roadmap-wrapper-<?php echo $moduleId; ?>-<?php echo htmlspecialchars((string) $index, ENT_QUOTES, 'UTF-8'); ?>"
+             data-index="<?php echo htmlspecialchars((string) $index, ENT_QUOTES, 'UTF-8'); ?>">
+          <div class="moa-roadmap-card">
+            <div class="moa-roadmap-card-title"><?php echo $item->options->above_dot_text ?? ''; ?></div>
           </div>
-				</div>
-			<?php endforeach; ?>
-		</div>
-	</div>
-	<!-- Area for showing Text Below Timeline dynamically -->
-    <div class="moa-roadmap-details">      
-      <?php foreach ($itemDescription as $index => $item): ?>
-        <div id='<?php echo "moa-roadmap-details-content-" . $index ?>' class="moa-roadmap-details-content-hidden">
-          <?php echo $item ?>
+          <div class="moa-roadmap-card-footer">
+            <span class="moa-roadmap-card-year"><?php echo htmlspecialchars($item->options->year ?? '', ENT_QUOTES, 'UTF-8'); ?></span>
+          </div>
         </div>
       <?php endforeach; ?>
     </div>
+  </div>
 
-<?php else: ?>
-	<p>No timeline items found. Please configure the module settings.</p>
+  <!-- Area for showing the text below the timeline dynamically -->
+  <div class="moa-roadmap-details">
+    <?php foreach ($descriptions as $index => $below) : ?>
+      <div class="moa-roadmap-details-content-hidden"
+           data-index="<?php echo htmlspecialchars((string) $index, ENT_QUOTES, 'UTF-8'); ?>">
+        <?php echo $below; ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+<?php else : ?>
+  <p><?php echo Text::_('MOD_HORIZONTAL_TIMELINE_NO_ITEMS'); ?></p>
 <?php endif; ?>
+</div>
